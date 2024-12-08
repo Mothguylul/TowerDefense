2b018a2d879b214e360b3c2bfd0449d2a4817cd8
mothguylul <juliusmuench89gmail.com>
2024-12-08 12:33:08 +0100

    root commit

---
+++ TowerDefense.Logic/Map.cs
@@ -0,0 +1,93 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.Remoting;
+
+namespace TowerDefense.Logic;
+
+public class Map
+{
+	private readonly object[,] _board;
+
+	public int Width => _board.GetLength(0);
+
+	public int Height => _board.GetLength(1);
+
+	public (int X, int Y) EnemySpawnPoint { get; private set; }
+	
+	public (int X, int Y) StartPoint {  get; private set; }
+
+	public (int X, int Y) EndPoint { get; private set; }
+
+	public List<(int x, int y)> Path => FindPath();
+
+	/// <summary>
+	/// The List of the valid Places at the Beginning
+	/// </summary>
+	public List<(int x, int y)> ValidStartTowerPlaces { get; private set; } 
+
+	/// <summary>
+	/// The List of the valid Tower Places through the Game, will be changed 
+	/// </summary>
+	public List<(int x, int y)> ValidTowerPlaces { get;  set; }
+
+	public List<(int x, int y)> PlacedTowers { get;  set; } //NOTE: later maybe with return value Tower
+
+	public Map(int width, int height, (int x, int y) enemySpawnPoint)
+	{
+		if (width <= 0 || height <= 0)
+			throw new ArgumentException(message: $"{nameof(Map)}.cs: width/height cannot be <= 0.");
+		else 
+			_board = new object[width, height];
+
+		// set enemyspawnpoint
+		if(enemySpawnPoint.y <= 0 || enemySpawnPoint.x <= 0)
+			throw new ArgumentException("Input cant be negative");
+		else
+			EnemySpawnPoint = enemySpawnPoint;
+
+		// Startpoint and EnemySpawnpoint will be at the same position
+		StartPoint = EnemySpawnPoint;
+	}
+
+
+	/// <summary>
+	/// Gets a Value on the Board by the given Y and X values
+	/// </summary>
+	/// <param name="y"></param>
+	/// <param name="x"></param>
+	/// <returns></returns>
+	/// <exception cref="ArgumentException"></exception>
+	public object GetBoardContents(int y, int x)
+	{
+		if (x < 0 || x >= Width || y < 0 || y >= Height)
+			throw new ArgumentException("Inputs must be valid");
+		else
+			return _board[y, x];
+	}
+
+
+	/// <summary>
+	/// Sets a Value to the given Y and X values
+	/// </summary>
+	/// <param name="y"></param>
+	/// <param name="x"></param>
+	/// <param name="value"></param>
+	/// <exception cref="ArgumentException"></exception>
+	public void SetBoardContents(int x, int y, object value)
+	{
+		if (x < 0 || x >= Width || y < 0 || y >= Height)
+			throw new ArgumentException("Inputs must be valid");
+		else
+			_board[y, x] = value;
+
+	}
+
+	/// <summary>
+	/// Finds the Path between the Start and Endpoint and returns it in a List
+	/// </summary>
+	/// <exception cref="NotImplementedException"></exception>
+	public (int x,int y) FindPath()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ TowerDefense.UnitTests/MapTests.cs
@@ -0,0 +1,90 @@
+using TowerDefense.Logic;
+using System.Linq;
+
+using FluentAssertions;
+namespace TowerDefense.UnitTests;
+
+public class MapTests
+{
+    [Theory]
+	[InlineData(10, 40)]
+	[InlineData(3,12)]
+	[InlineData(13,32)]
+	public void ints_Should_Match_Size_Of_Array(int expectedWidth, int expectedHeight)
+    {
+        //Arrange
+        Map map = new Map(expectedWidth, expectedHeight, (3,4));
+
+        //Act
+        int actualWidth = map.Width;
+        int actualHeight = map.Height;
+
+		//Assert
+		actualWidth.Should().Be(expectedWidth);
+		actualHeight.Should().Be(expectedHeight);
+	}
+	[Theory]
+	[InlineData(0, 0)]
+	[InlineData(0, 1)]
+	[InlineData(1, 0)]
+	[InlineData(10, -5)]
+	[InlineData(-5, 10)]
+	public void Construction_ShouldThrowArgumentException_WhenInvalidMapSize(int attemptedWidth, int attemptedHeight)
+	{
+		// Arrange
+		Action create = () => new Map(attemptedWidth, attemptedHeight, (3,4));
+
+		// Act
+
+		// Assert
+		create.Should().ThrowExactly<ArgumentException>().WithMessage("*cannot be <= 0*");
+	}
+
+	[Theory]
+	[InlineData(3, 3, 5)]
+	[InlineData(4, 4, false)]
+	[InlineData(0, 0, 3.2f)]
+	public void GetAndSetBoardContents_ShouldWorkCorrectly_WithValidInput(int x, int y, object expectedValue)
+	{
+		//Arrange
+		Map map = new Map(10,10, (3,4));
+
+		//Act
+		map.SetBoardContents(x, y, expectedValue);
+		var actualValue = map.GetBoardContents(y, x);
+
+		//Assert
+		Assert.Equal(expectedValue, actualValue);
+	}
+
+	[Theory]
+	[InlineData(10, 10, 4, 4)]
+	public void EnemySpawnPoint_Should_Be_Correctly_Passed(int x, int y, int enemySpawnPointX, int enemySpawnPointY)
+	{
+
+		//Arrange
+		(int, int) enemySpawnPoint = new(enemySpawnPointX, enemySpawnPointY);
+		Map map = new Map(x, y, enemySpawnPoint);
+
+		//Act
+
+		//Assert
+		map.EnemySpawnPoint.Should().Be(enemySpawnPoint);
+
+	}
+	[Theory]
+	[InlineData(10, 10, -7, 1)]
+	[InlineData(10, 10, 3, -3)]
+	[InlineData(10, 10, -7, -7)]
+	public void EnemySpawnPoint_Should_ThrowArgumentException_WhenInvalid(int x, int y, int enemySpawnPointX, int enemySpawnPointY)
+	{
+		// Arrange
+		(int, int) enemySpawnPoint = (enemySpawnPointX, enemySpawnPointY);
+
+		// Act
+		Action act = () => new Map(x, y, enemySpawnPoint);
+
+		// Assert
+		act.Should().Throw<ArgumentException>().WithMessage("*Input cant be negative*");
+	}
+}
