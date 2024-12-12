53e4d0353d69bc9e42d054fcf7fe349d7c3e3a13
mothguylul <juliusmuench89gmail.com>
2024-12-12 18:56:59 +0100

    Wavemanager.cs Base, added Enemy.cs, Gamecontroller.cs, Wave.cs, Tower.cs

---
+++ TowerDefense.Logic/Wave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Logic
+{
+	public class Wave
+	{
+		/// <summary>
+		/// List of the type of the Enemys and of the amount of Enemys in that wave
+		/// </summary>
+		public readonly Dictionary<Enemy, int> TypeAndAmountOfWave = new Dictionary<Enemy, int>();
+	}
+}
---
+++ TowerDefense.Logic/WaveManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefense.Logic;
+
+namespace TowerDefense.Logic
+{
+	public class WaveManager
+	{
+		public bool isWaveEnded { get; private set; } = false;
+
+		public bool isWaveStarted { get; private set; }
+
+		public List<Wave> Waves { get; set; }
+
+		public int WavesCount { get; set; } = 0;
+
+		public WaveManager(List<Wave> waves, int wavesCount = 0) 
+		{
+			Waves = waves;
+			WavesCount = wavesCount;
+		}
+	}
+}
